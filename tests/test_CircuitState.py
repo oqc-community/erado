a626a772fb82b223a3f0cf294dc9e2d4b4676a04
Sam Griffiths <sgriffiths@oqc.tech>
2025-10-13 16:31:05 +0100

    Added tests for EOL measurements and multiprocessing deadlock bug

--- tests/test_CircuitState.py
+++ tests/test_CircuitState.py
@@ -3,16 +3,19 @@ from erado.models import CircuitState
 import pytest
 
 
+N = 5
+"""Number of qubits to use in these tests, unless otherwise specified."""
+
+
 def test_serialise():
-    num_qubits = 5
-    state = CircuitState(erasure="0"*num_qubits, measure="1"*num_qubits)
+    state = CircuitState(erasure="0"*N, measure="1"*N)
     state_str = state.model_dump_json()
     assert state_str == "\"00000,11111\""
 
 
 @pytest.mark.parametrize("state_str,expected", [
-    (f"\"{"0"*5},{"1"*5}\"", ("0"*5, "1"*5)),  # Measure all qubits
-    (f"\"{"0"*5},{"1"*2}\"", ("0"*5, "1"*2))   # Measure only some qubits
+    (f"\"{"0"*N},{"1"*N}\"", ("0"*N, "1"*N)),  # Measure all qubits
+    (f"\"{"0"*N},{"1"*2}\"", ("0"*N, "1"*2))   # Measure only some qubits
 ])
 def test_deserialise(state_str: str, expected: tuple[str, str]):
     state = CircuitState.model_validate_json(state_str)
@@ -22,9 +25,9 @@ def test_deserialise(state_str: str, expected: tuple[str, str]):
 
 
 @pytest.mark.parametrize("state_str,expected", [
-    ("0"*5 + "1"*5, ("0"*5, "1"*5)),    # Measure all qubits
-    ("0 "*5 + "1 "*5, ("0"*5, "1"*5)),  # Arbitrary whitespace from qiskit
-    ("0"*5 + "1"*2, ("0"*5, "1"*2))     # Measure only some qubits
+    ("0"*N + "1"*N, ("0"*N, "1"*N)),    # Measure all qubits
+    ("0 "*N + "1 "*N, ("0"*N, "1"*N)),  # Arbitrary whitespace from qiskit
+    ("0"*N + "1"*2, ("0"*N, "1"*2))     # Measure only some qubits
 ])
 def test_from_string(state_str: str, expected: tuple[str, str]):
     state = CircuitState.from_string(state_str)
---
+++ tests/test_frontend.py
@@ -0,0 +1,86 @@
+from erado.models import ErasureModel, ErasurePassJob, ErasureCircuitSampler
+from erado.frontend import ErasureSimFrontend, ErasureSimFrontendResults
+from erado.circuits import ghz_circuit
+
+from qiskit import ClassicalRegister
+from qiskit_aer import AerSimulator
+
+import pytest
+
+
+N = 4
+"""Number of qubits to use in these tests, unless otherwise specified."""
+
+
+@pytest.mark.parametrize("model_type", [ErasureCircuitSampler, ErasurePassJob])
+def test_measure_EOL_all(model_type: type[ErasureModel]):
+    """
+    Test basic simulator usage, assuming end-of-line measurements on all qubits.
+    """
+    circuit = ghz_circuit(N)
+    circuit.measure_all()
+
+    backend = AerSimulator(method="statevector", device="CPU")
+    shots = 10
+    erasure_rate = 50e-6
+
+    model = model_type(circuit=circuit, erasure_rate=erasure_rate)
+    frontend = ErasureSimFrontend(model=model)
+    results = frontend.run(backend, shots)
+
+    assert isinstance(results, ErasureSimFrontendResults)
+    assert results.shots == shots
+    assert results.counts.total() == shots
+
+
+@pytest.mark.parametrize("model_type", [ErasureCircuitSampler, ErasurePassJob])
+def test_measure_EOL_some(model_type: type[ErasureModel]):
+    """
+    Test basic simulator usage, assuming end-of-line measurements on only some qubits.
+    """
+    circuit = ghz_circuit(N)
+
+    circuit.add_register(ClassicalRegister(N-2, "meas"))
+    circuit.barrier()
+    for i in range(N-2):
+        circuit.measure(i, i)
+
+    backend = AerSimulator(method="statevector", device="CPU")
+    shots = 10
+    erasure_rate = 50e-6
+
+    model = model_type(circuit=circuit, erasure_rate=erasure_rate)
+    frontend = ErasureSimFrontend(model=model)
+    results = frontend.run(backend, shots)
+
+    assert isinstance(results, ErasureSimFrontendResults)
+    assert results.shots == shots
+    assert results.counts.total() == shots
+    pass
+
+
+def test_multiprocessing_deadlock():
+    """
+    Test for regressions in the Qiskit multiprocessing bug.
+
+    Qiskit seems to introduce some buggy behaviour in backend runs due to its own use of
+    multithreading. If a backend simulation job is performed before a multiprocess-enabled
+    `ErasureCircuitSampler` run, this run can deadlock, even if a new backend object is created.
+
+    This was originally only 'solved' by performing simulation runs in strictly different programs.
+    However, it now seems that I've fixed it by some careful use of multiprocessing context
+    settings. This test is designed to catch any regressions in this bugfix.
+    """
+    circuit = ghz_circuit(N)
+    circuit.measure_all()
+
+    backend = AerSimulator(method="statevector", device="CPU")
+    shots = 10
+    erasure_rate = 50e-6
+
+    circuit_sampler = ErasureCircuitSampler(circuit=circuit, erasure_rate=erasure_rate)
+    pass_job = ErasurePassJob(circuit=circuit, erasure_rate=erasure_rate)
+
+    circuit_sampler.run(backend, shots)
+    pass_job.run(backend, shots)
+    circuit_sampler.run(backend, shots)
