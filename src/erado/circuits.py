fea15601a941e5637ff6a02147a705a4aa89d51d
Sam Griffiths <sgriffiths@oqc.tech>
2025-10-13 20:28:18 +0100

    Finished documenting circuits.py

--- src/erado/circuits.py
+++ src/erado/circuits.py
@@ -4,6 +4,14 @@ from qiskit import QuantumCircuit
 
 
 def ghz_circuit(n: int) -> QuantumCircuit:
+    """Greenberger-Horne-Zeilinger (GHZ) state preparation.
+
+    Args:
+        n: Number of qubits.
+
+    Returns:
+        A new Qiskit quantum circuit.
+    """
     qc = QuantumCircuit(n)
     qc.h(0)
     for i in range(n - 1):
