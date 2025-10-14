f76c4e25dd4392a2962af0b6f83e8c641d661b97
Sam Griffiths <sgriffiths@oqc.tech>
2025-10-14 17:32:33 +0100

    Finished documenting util.py

--- src/erado/util.py
+++ src/erado/util.py
@@ -6,13 +6,10 @@ import multiprocessing
 
 
 class MultiprocessingRNG:
-    """
-    Mixin class providing a NumPy random number generator (RNG) suitable for multiprocessing.
+    """Mixin class providing a NumPy random number generator (RNG) suitable for multiprocessing.
 
-    The `seed` public method seeds the RNG by reconstructing it with the given argument.
-    The `_reseed` private method reconstructs the RNG with fresh entropy if the current seed
-    is `None`, or with a deterministically-advanced seed if the seed is an integer, suitable for
-    use in initialiser functions for child processes.
+    Via the `_reseed` protected method, this allows for child processes to each have seeds which
+    are unique yet deterministic w.r.t. the primary seed (or fully random if the seed is `None`).
     """
     _seed: int | None = None
     _rng: np.random.Generator = np.random.default_rng()
@@ -20,11 +17,22 @@ class MultiprocessingRNG:
 
     @classmethod
     def seed(cls, seed: int | None = None) -> None:
+        """Seed the RNG by reconstructing it with the given argument.
+
+        Args:
+            seed: Seed value to be passed to NumPy RNG.
+        """
         cls._seed = seed
         cls._rng = np.random.default_rng(seed)
 
     @classmethod
     def _reseed(cls) -> None:
+        """Reseed this RNG instance.
+
+        This reconstructs the RNG with fresh entropy if the current seed is `None`, or with a
+        deterministically-advanced seed if the seed is an integer, suitable for use in initialiser
+        functions for child processes.
+        """
         with cls._reseed_counter.get_lock():
             cls._reseed_counter.value += 1
 
