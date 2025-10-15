11a26b02b390d6690d22939757ea365e8cb6f179
Sam Griffiths <sgriffiths@oqc.tech>
2025-10-15 14:03:46 +0100

    Added indent/dedent functions

--- make_stub_file.py
+++ make_stub_file.py
@@ -9,12 +9,31 @@ from io import TextIOWrapper
 
 
 class FileWriter:
+    INDENT_SIZE = 4
+
     def __init__(self, out: TextIOWrapper):
         self.out = out
+        self._level = 0
+
+    def indent(self) -> None:
+        self._level += 1
+
+    def dedent(self) -> None:
+        if self._level == 0:
+            raise RuntimeError("Cannot dedent below level 0.")
+        self._level -= 1
 
     def write(self, s: str):
         s_sanitised = s.replace("\\", "")
-        self.out.write(s_sanitised)
+        if s_sanitised == "\n":
+            self.out.write(s_sanitised)
+        else:
+            s_indented = (self._indent_str() * self._level) + s_sanitised
+            self.out.write(s_indented)
+
+    @classmethod
+    def _indent_str(cls) -> str:
+        return " " * cls.INDENT_SIZE
 
 
 EXEMPT_MEMBERS: list[str] = [
@@ -43,7 +62,8 @@ with open('typings/qiskit/_accelerate/circuit.pyi', 'w') as file:
                     f.write(cls.__name__)
                 f.write("):\n")
 
-            f.write(f"    \"\"\"{obj.__doc__}\"\"\"\n")
+            f.indent()
+            f.write(f"\"\"\"{obj.__doc__}\"\"\"\n")
 
             for member_name, member in inspect.getmembers(obj):
                 if member_name not in EXEMPT_MEMBERS:
@@ -55,21 +75,23 @@ with open('typings/qiskit/_accelerate/circuit.pyi', 'w') as file:
 
                     # Methods
                     if inspect.ismethoddescriptor(member):
-                        f.write(f"    def {member_name}{inspect.signature(member)}:\n")
+                        f.write(f"def {member_name}{inspect.signature(member)}:\n")
+                        f.indent()
                         if member.__doc__ != "":
-                            f.write(f"        \"\"\"{member.__doc__}\"\"\"\n")
-                        f.write("        ...\n")
+                            f.write(f"\"\"\"{member.__doc__}\"\"\"\n")
+                        f.write("...\n")
+                        f.dedent()
 
                     # Data descriptor
                     elif inspect.isdatadescriptor(member):
-                        f.write(f"    {member_name}: Incomplete  # DATA DESCRIPTOR\n")
+                        f.write(f"{member_name}: Incomplete  # DATA DESCRIPTOR\n")
                         if member.__doc__ != "":
-                            f.write(f"    \"\"\"{member.__doc__}\"\"\"\n")
+                            f.write(f"\"\"\"{member.__doc__}\"\"\"\n")
 
                     # Plain-old attribute
                     else:
                         if member is not None:
-                            f.write(f"    {member_name}: {type(member).__name__}\n")
+                            f.write(f"{member_name}: {type(member).__name__}\n")
 
                     # f.write(f"# ismethoddescriptor = {inspect.ismethoddescriptor(member)}\n")
                     # f.write(f"# ismethod = {inspect.ismethod(member)}\n")
@@ -82,8 +104,11 @@ with open('typings/qiskit/_accelerate/circuit.pyi', 'w') as file:
 
             names: list[str] = [tup[0] for tup in inspect.getmembers(obj)]
             if "__getitem__" in names and "__iter__" not in names:
-                print(f"Old-style Iterable: {name}")
                 f.write("\n")
-                f.write("    def __iter__(self):\n")
-                f.write("        \"\"\"MOCKED TO MAKE THIS AN OLD-STYLE ITERABLE!\"\"\"\n")
-                f.write("        ...\n")
+                f.write("def __iter__(self):\n")
+                f.indent()
+                f.write("\"\"\"MOCKED TO MAKE THIS AN OLD-STYLE ITERABLE!\"\"\"\n")
+                f.write("...\n")
+                f.dedent()
+
+            f.dedent()
