dbee301f27672b4c335ed43ce09a1c75ee52c86b
Sam Griffiths <sgriffiths@oqc.tech>
2025-10-30 18:11:14 +0000

    Removed calculate_fidelity_old

--- src/erado/fidelity.py
+++ src/erado/fidelity.py
@@ -3,7 +3,6 @@
 
 from erado.models import (
     CircuitState,
-    postselect_counts,
     ShotInfo,
 )
 from erado.util import NPVector
@@ -13,7 +12,6 @@ from qiskit.quantum_info import Statevector
 
 import numpy as np
 
-from collections import Counter
 from collections.abc import Generator
 from dataclasses import dataclass
 import logging
@@ -36,24 +34,6 @@ def calculate_pdf(circuit: QuantumCircuit) -> NPVector[np.float64]:
     psi = Statevector(circuit_pure)
     pdf = np.abs(psi)**2
     return pdf
-
-
-def calculate_fidelity_old(counts: Counter[CircuitState], circuit: QuantumCircuit) -> float:
-    # Calculate the exact non-noisy probability density function
-    pdf_ideal = calculate_pdf(circuit)
-
-    # Calculate the empirical noisy probability density function
-    counts_noisy = postselect_counts(counts)
-    shots = counts_noisy.total()
-    n = circuit.num_qubits
-    states = (format(i, f"0{n}b") for i in range(2**n))
-    pdf_noisy = np.fromiter((counts_noisy[state] / shots
-                             for state in states),
-                            dtype=np.float64)
-
-    # Calculate the fidelity (i.e. square of the Bhattacharyya coefficient)
-    fidelity = np.inner(np.sqrt(pdf_ideal), np.sqrt(pdf_noisy))**2
-    return fidelity
 
 
 def calculate_fidelity(psi: Statevector, ideal: QuantumCircuit | Statevector | NPVector[np.float64]) -> float:
