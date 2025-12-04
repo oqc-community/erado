8836519fce25c0c6269e690770903b16207d56d8
Sam Griffiths <sgriffiths@oqc.tech>
2025-12-04 14:35:48 +0000

    Ensured SharedMemoryManager uses spawn not fork

--- src/erado/frontend.py
+++ src/erado/frontend.py
@@ -12,6 +12,7 @@ import pydantic
 import numpy as np
 
 from collections import Counter
+import multiprocessing
 from multiprocessing.managers import SharedMemoryManager
 
 
@@ -175,7 +176,7 @@ class ErasureSimFrontend(util.MultiprocessingRNG):
         Returns:
             Data structure of results and statistics from the simulation.
         """
-        with SharedMemoryManager() as smm:
+        with SharedMemoryManager(ctx=multiprocessing.get_context("spawn")) as smm:
             # A new FidelityFunctor is needed for each postselection round
             # (as they are effectively distinct simulations, each one unaware of the last)
             fidelity_functors = list[fidelity.FidelityFunctor]()
