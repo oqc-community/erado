1bc1070fae52a148a1aab70d24526b44e7db4b15
Sam Griffiths <sgriffiths@oqc.tech>
2026-03-04 16:25:21 +0000

    Refactored conf.py to match project standards

--- docs/conf.py
+++ docs/conf.py
@@ -1,27 +1,36 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""Configuration file for the Sphinx documentation builder.
+
+For the full list of built-in configuration values, see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'erado'
-copyright = '2026, Oxford Quantum Circuits Ltd'
-author = 'Sam J. Griffiths'
+project = "erado"
+copyright = "2026, Oxford Quantum Circuits Ltd"
+author = "Sam J. Griffiths"
+
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = [
+    "_templates",
+]
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = [
+    "_static",
+]
